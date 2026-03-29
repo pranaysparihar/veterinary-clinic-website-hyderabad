718d41d4028561e1a2f597c3328d501c7ec4539a
Pranay Singh Parihar <pranaysparihar@Pranays-MacBook-Pro.local>
2026-03-30 04:05:44 +0530

    configure github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: '/veterinary-clinic-website-hyderabad/',
   resolve: {
     alias: {
       // Alias @ to the src directory
